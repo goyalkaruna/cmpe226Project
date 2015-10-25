04266237e201733e32a63bc55c1a4c857aaacf90
goyalkaruna <goyalkaruna@yahoo.co.in>
2015-10-24 22:30:18 -0700

    Initial Base commit

---
+++ userLogin.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>IJK(India Korea Japan</title>
+
+<script type="text/javascript" src="bootstrap.js"></script>
+<script type="text/javascript" src="bootstrap.min.js"></script>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<style>
+.navbar-default1 {
+    background-color: #F8F8F8;
+    border-color: #E7E7E7;
+}
+.navbar-custom {
+    background-color:#4682B4;
+    color:#ffffff;
+    border-radius:0;
+}
+
+.navbar-custom .navbar-nav > li > a {
+    color:#fff;
+}
+.navbar-custom .navbar-nav > .active > a, .navbar-nav > .active > a:hover, .navbar-nav > .active > a:focus {
+    color: #ffffff;
+    background-color:transparent;
+}
+.navbar-custom .navbar-brand {
+    color:#eeeeee;
+}
+</style>
+</head>
+<body align="center">
+<nav class="navbar navbar-custom">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">IJK</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Music App <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Hi User</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">My Account</a></li>
+            <li><a href="#">My History</a></li>
+            <li><a href="#">My Jukebox</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">About</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">My Account</a></li>
+            <li><a href="#">My History</a></li>
+            <li><a href="#">My Jukebox</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<?php
+   
+      try{
+           // Connect to the database.
+           $con = new PDO("mysql:host=localhost;dbname=assignment04",
+                           "root", "root");
+            $con->setAttribute(PDO::ATTR_ERRMODE,
+                               PDO::ERRMODE_EXCEPTION);
+
+
+             $email     = filter_input(INPUT_POST, "email");   
+             $password     = filter_input(INPUT_POST, "password");
+            
+
+             $query = "SELECT * FROM User WHERE email = '$email' AND   password  = '$password'";
+               
+                // Fetch the database field names.
+                $result = $con->query($query);
+                $row = $result->fetch(PDO::FETCH_ASSOC);
+                
+                if($row <= 0){
+                  print "<p>Username or password is invalid!!<br></p>";
+                }
+                else{
+                $data = $con->query($query);
+                $data->setFetchMode(PDO::FETCH_ASSOC);
+                
+                 header("Location: /home.html");
+                  exit;
+
+                }
+
+             
+    
+         }
+        catch(PDOException $ex) {
+            echo 'ERROR: '.$ex->getMessage();
+        }
+         
+      ?>
+   </body>
+</html>
