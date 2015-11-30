d1097f4544ef60ad6e0b91873c1ebcfb5aafa2e6
root <goyalkaruna@yahoo.com>
2015-11-30 01:21:47 -0800

    few changes

--- album.php
+++ album.php
@@ -1,69 +1,62 @@
+<?php
+session_start();
+if(!isset($_SESSION['sess_name'])){
+      header('Location: login.php?err=2');
+}
+?>
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="utf-8" />
+<title>IJK(India Korea Japan</title>
 
 <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
 <link href="MusicApp.css" rel="stylesheet">
-    <title>Albums</title>
+
 </head>
-
 <body>
-  <nav class="navbar navbar-custom">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">IJK</a>
-      </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Music App <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Hi User</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">My Account</a></li>
-              <li><a href="#">My History</a></li>
-              <li><a href="#">My Jukebox</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">About</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">My Account</a></li>
-              <li><a href="#">My History</a></li>
-              <li><a href="#">My Jukebox</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+<nav class="navbar navbar-custom">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="home.php">IJK Music App</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+
+        <li class="username"><a class="active" href="home.php"><B>Hi <?php  echo $_SESSION['sess_name']; ?> </B></a></li>
+	
+
+      </ul>
+
+      <ul class="nav navbar-nav navbar-left active">
+        <li class="active"><a href="upload.php">Upload an Album <span class="glyphicon glyphicon-upload" aria-hidden="true"></span></a></li>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-left active">
+        <li class="active"><a href="albums.php">My Albums <span class="glyphicon glyphicon-folder" aria-hidden="true"></span></a></li>
+	<li><a href="history.php">My History</a></li>
+      </ul>
+<ul class="nav navbar-nav navbar-right">
+  <li class="active"><a href="search.html"   role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+  <li><a href="logout.php">logout <span class="glyphicon glyphicon-user" aria-hidden="true"></a></li>
+
+</ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>><!-- /.container-fluid -->
+
   <div class="container">
 <h3><p class="lead"><I>Choose your Album !!!</I> </p></h3>
 
@@ -72,11 +65,10 @@
            include ('include/dbConnect.php');
 
 
-                    $query = "SELECT * FROM album, image , publisher WHERE album.cover_image_id = image_id AND publisher.publisher_id = album.publisher_id";
+                    $query = "SELECT * FROM Album, Image , Publisher WHERE Album.cover_image_id = Image.image_id AND Publisher.publisher_id = Album.publisher_id";
 
 
                 // We're going to construct an HTML table.
-                print "<table>\n";
 
                 // Query the database.
                 $data = $con->query($query);
@@ -96,11 +88,11 @@
                           echo "<tr>";
                           echo "<td>".$row['album_id']."</td>";
                           echo "<td><img src =".$row['data']."></td>";
-                          echo "<td>".$row['title']."</td>";
+                          echo "<td>".$row['album_title']."</td>";
                           echo "<td>".$row['name']."</td>";
                           echo "<td>".$row['released_date']."</td>";
 
-                          echo "<td><a>Songs</a></td>";
+                          echo "<td><a href='song.php?album_id=".$row['album_id']."'>Songs</a></td>"; 
                           echo "</tr>\n";
                       }
 
--- artist.php
+++ artist.php
@@ -1,69 +1,60 @@
+<?php
+session_start();
+if(!isset($_SESSION['sess_name'])){
+      header('Location: login.php?err=2');
+}
+?>
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="utf-8" />
+<title>IJK(India Korea Japan</title>
 
 <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
 <link href="MusicApp.css" rel="stylesheet">
-    <title>Artists</title>
+
 </head>
-
 <body>
-  <nav class="navbar navbar-custom">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">IJK</a>
-      </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Music App <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Hi User</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">My Account</a></li>
-              <li><a href="#">My History</a></li>
-              <li><a href="#">My Jukebox</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">About</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">My Account</a></li>
-              <li><a href="#">My History</a></li>
-              <li><a href="#">My Jukebox</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+<nav class="navbar navbar-custom">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="home.php">IJK Music App</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+
+        <li class="username"><a class="active" href="home.php"><B>Hi <?php  echo $_SESSION['sess_name']; ?> </B></a></li>
+	<li><a href="history.php">My History</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-left active">
+        <li class="active"><a href="upload.php">Upload an Album <span class="glyphicon glyphicon-upload" aria-hidden="true"></span></a></li>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-left active">
+        <li class="active"><a href="album.php">My Albums <span class="glyphicon glyphicon-folder" aria-hidden="true"></span></a></li>
+
+      </ul>
+<ul class="nav navbar-nav navbar-right">
+  <li class="active"><a href="search.html"   role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+  <li><a href="logout.php">logout <span class="glyphicon glyphicon-user" aria-hidden="true"></a></li>
+
+</ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
   <div class="container">
     <h3><p class="lead"><I>Choose your Artist !!!</I> </p></h3>
 
@@ -72,18 +63,22 @@
           include ('include/dbConnect.php');
 
 
-                    $query = "SELECT * FROM artist";
+                    $query = "SELECT * FROM Artist a, Album b, ArtistAlbum c where a.artist_id = c.artist_id and b.album_id = c.album_id";
                     // Query the database.
+       //echo "<td><a href="songs.php?id=$row['artist_id']">Songs</a></td>";
                     $data = $con->query($query);
                     $data->setFetchMode(PDO::FETCH_ASSOC);
 
                     echo "<table>";
-                    echo "<tr><th>Artist Name</th><th>List</th></tr>";
+                    echo "<tr><th>Artist Name</th><th>Album Name</th><th>Released On</th><th>Details</th></tr>";
                     foreach( $data as $row) {
                         echo "<tr>";
                         echo "<td>".$row['artist_id']."</td>";
                         echo "<td>".$row['name']."</td>";
-                        echo "<td><a>View My Albums</a></td>";
+			echo "<td>".$row['album_title']."</td>";
+			echo "<td>".$row['released_date']."</td>";
+			echo "<td><a href='song.php?artist_id=".$row['artist_id']."'>songs</a></td>"; 
+                 
                         echo "</tr>\n";
                     }
 
